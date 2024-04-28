b97a50bdd587b95df53db15594dc945e6da7f443
Kusuma Gigih Prakoso <101495147+kusumagigih@users.noreply.github.com>
2024-04-29 00:02:53 +0700

    Edited button

--- resources/views/admin/blogs/edit.blade.php
+++ resources/views/admin/blogs/edit.blade.php
@@ -1,6 +1,6 @@
 <x-app-form-layout :header="$blog->id == 0 ? 'Publish Blog' : 'Edit Blog'">
     <x-slot name="pageButtons">
-        <button form="myform" class="btn btn-primary">
+        <button form="myform" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"">
             Simpan
         </button>
     </x-slot>
